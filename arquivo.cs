9387a4b7b81d36575c1c97be1f16866a95ac5a51
joao vitor <joaovitorbatista931@gmail.com>
2022-03-09 15:52:28 -0300

    visual joao vitor c#

---
+++ arquivo.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string firstName = "caua" ;
+      string lastName = " vitor " ;
+      string fullName = firstName + lastName;
+      Console.WriteLine (fullName);
+    }
+  }
+}
